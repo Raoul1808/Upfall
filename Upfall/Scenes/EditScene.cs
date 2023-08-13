aaeda0432c8d345d61b28bee83206c98bc91728e
Raoul1808 <raoulthegeek@gmail.com>
2023-08-13 19:54:10 +0200

    "Level editor" scene with player

--- Upfall/Scenes/EditScene.cs
+++ Upfall/Scenes/EditScene.cs
@@ -5,6 +5,8 @@ using Brocco.Input;
 using Brocco.Util;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Upfall.Entities;
 
 namespace Upfall.Scenes;
 
@@ -13,14 +15,23 @@ public class EditScene : Scene
     private Tilemap _tilemap;
     private Size _tilemapSize;
     private Point _currentTilePos;
+    private Player _player;
+    private bool _updatePlayer = false;
     
     public override void Load()
     {
+        _player = AddToScene<Player>();
         _tilemap = new Tilemap(_tilemapSize = new Size(40, 23));
+        PauseUpdate = true;
     }
 
     public override void Update(float dt)
     {
+        if (InputManager.GetKeyPress(Keys.Enter))
+            PauseUpdate = !PauseUpdate;
+
+        _tilemap.SolveCollisions(_player);
+        
         var pos = InputManager.GetCanvasMousePosition();
         int x = BroccoMath.Clamp((int)pos.X / Tilemap.TileSize, 0, _tilemapSize.Width - 1);
         int y = BroccoMath.Clamp((int)pos.Y / Tilemap.TileSize, 0, _tilemapSize.Height - 1);
@@ -29,11 +40,17 @@ public class EditScene : Scene
             _tilemap.SetTile(_currentTilePos, 1);
         if (InputManager.GetClickDown(MouseButtons.Right))
             _tilemap.SetTile(_currentTilePos, 0);
+        if (InputManager.GetClickDown(MouseButtons.Middle))
+        {
+            _player.Position = pos;
+            _player.Velocity = Vector2.Zero;
+        }
     }
 
     public override void CanvasRender(SpriteBatch spriteBatch)
     {
         _tilemap.Render(spriteBatch);
+        _player.Render(spriteBatch);
         int s = Tilemap.TileSize;
         spriteBatch.Draw(Assets.Pixel, new Rectangle(_currentTilePos.X * s, _currentTilePos.Y * s, s, s), Color.DimGray * 0.5f);
     }
