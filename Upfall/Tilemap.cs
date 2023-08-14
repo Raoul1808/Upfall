bf8d18e0b2c996f01bb4d7efb34473d066f06d97
Raoul1808 <raoulthegeek@gmail.com>
2023-08-14 22:42:56 +0200

    Better-ish collision detection?

--- Upfall/Tilemap.cs
+++ Upfall/Tilemap.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using Brocco;
 using Brocco.Basic;
+using Brocco.Util;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Upfall.Entities;
@@ -26,6 +28,10 @@ public class Tilemap
     {
         _tiles[pos.Y, pos.X] = tile;
     }
+
+    public Rectangle GetTileRect(Point pos) => GetTileRect(pos.X, pos.Y);
+
+    public Rectangle GetTileRect(int x, int y) => new(x * TileSize, y * TileSize, TileSize, TileSize);
     
     public const int TileSize = 16;
 
@@ -64,8 +70,10 @@ public class Tilemap
         writer.Flush();
         writer.Dispose();
     }
+
+    private string RectToStr(Rectangle rect) => $"{{Left: {rect.Left}, Right: {rect.Right}, Top: {rect.Top}, Bottom: {rect.Bottom}}}";
     
-    public void SolveCollisions(Player entity)
+    public void SolveCollisions(Entity entity)
     {
         var pos = entity.Position / TileSize;
         int lx = Math.Max((int)pos.X - 1, 0);
@@ -73,12 +81,60 @@ public class Tilemap
         int ly = Math.Max((int)pos.Y - 1, 0);
         int uy = Math.Min((int)pos.Y + 2, _tiles.GetLength(0) - 1);
 
+        var tileRects = new List<Tuple<float, Rectangle>>();
+
         for (int x = lx; x <= ux; x++)
         {
             for (int y = ly; y <= uy; y++)
             {
-                if (_tiles[y, x] != 0)
-                    entity.ResolveTileCollision(new Rectangle(x * TileSize, y * TileSize, TileSize, TileSize));
+                var bbox = entity.BoundingBox;
+                var tileRect = GetTileRect(x, y);
+                if (_tiles[y, x] != 0 && bbox.Intersects(tileRect))
+                {
+                    tileRects.Add(new(BroccoMath.Distance(bbox.Center, tileRect.Center), tileRect));
+                }
+            }
+        }
+
+        if (tileRects.Count <= 0) return;  // No collision detected?
+        
+        tileRects.Sort((x, y) => x.Item1.CompareTo(y.Item1));
+
+        foreach (var (_, tileRect) in tileRects)
+        {
+            // TODO: the collision code REALLY shouldn't be here, but I REALLY NEED to make progress in the game.
+            if (!entity.BoundingBox.Intersects(tileRect)) continue;
+
+            var depth = RectangleExtensions.GetIntersectionDepth(entity.BoundingBox, tileRect);
+
+            float xDepthAbs = Math.Abs(depth.X);
+            float yDepthAbs = Math.Abs(depth.Y);
+
+            if (yDepthAbs < xDepthAbs)
+            {
+                if (entity.TouchedTopOf(tileRect))
+                {
+                    entity.Position.Y = tileRect.Top - entity.BoundingBox.Height / 2f;
+                    entity.Velocity.Y = 0;
+                }
+
+                if (entity.TouchedBottomOf(tileRect))
+                {
+                    entity.Position.Y = tileRect.Bottom + entity.BoundingBox.Height / 2f;
+                    entity.Velocity.Y = 0;
+                }
+            }
+            
+            if (entity.TouchedLeftOf(tileRect))
+            {
+                entity.Position.X = tileRect.Left - entity.BoundingBox.Width / 2f;
+                entity.Velocity.X = 0;
+            }
+            
+            if (entity.TouchedRightOf(tileRect))
+            {
+                entity.Position.X = tileRect.Right + entity.BoundingBox.Width / 2f;
+                entity.Velocity.X = 0;
             }
         }
     }
