ff8e97cd25ab7dfd4f061d4e18c1ceab227b6297
Raoul1808 <raoulthegeek@gmail.com>
2023-08-18 17:28:45 +0200

    Remove mkdir command from linux compile script

--- compile-release-linux.sh
+++ compile-release-linux.sh
@@ -1,4 +1,3 @@
 dotnet restore -r linux-x64
 dotnet publish -r linux-x64 -c Release
-mkdir Upfall/bin/Release/net7.0/linux-x64/publish/netcoredeps
 cp -r Brocco/deps/linux-x64/. Upfall/bin/Release/net7.0/linux-x64/publish/netcoredeps/
